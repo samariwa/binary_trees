b613a5a3db26381471e6840620ddfd0f3158e699
“samariwa” <samuelmariwa@gmail.com>
2022-12-14 21:23:05 +0300

    Post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -15,7 +15,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
 }
